baef11a4cbcbbed4911117b9e68c93143bdb179a
JayKadam9 <er.jayk@gmail.com>
2016-03-06 18:39:47 +0530

    My Assignment No2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+cacheSolve <- function(NewMatrix=matrix()) 
+{
+        #fetches the cached inverse, if already calculated
+        cachedInverse <- NewMatrix$getInverseMatrix()
+        
+        #checks if the fetched value is NULL or not
+        if(!is.null(cachedInverse)) 
+        {
+                message("Getting Cached Inverse for Matrix")
+                return(cachedInverse)
+        }
+        
+        #gets the value of NewMatrix, as it is not already cached
+        fetchMatrix <- NewMatrix$getMatrix()
+        
+        #calculates the inverse of New Matrix
+        cachedInverse <- solve(fetchMatrix)
+        
+        #caches the inverse matrix for future use
+        NewMatrix$setInverseMatrix(cachedInverse)
+        
+        #returns value of inverse
+        cachedInverse
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+makeCacheMatrix <- function(NewMatrix) 
+{
+        # Holds the cached Inverse of Matrix
+        # Initially nothing calculated hence assigned NULL
+        cachedInverse <- NULL
+        
+        #sets the Matrix
+        setMatrix <- function(NewMatrix) 
+        {
+                NewMatrix <<- NewMatrix
+                
+                # Initially nothing calculated hence assigned NULL
+                cachedInverse <<- NULL
+        }
+        
+        #gets the Matrix
+        getMatrix <- function()
+        {
+                NewMatrix  
+        }
+        
+        #caches the Inversed Matrix for future use        
+        setInverseMatrix <- function(cachedInverse) 
+        { 
+                cachedInverse <<- cachedInverse
+        }
+        
+        #returns Inverse Matrix
+        getInverseMatrix <- function()
+        {
+                cachedInverse
+        }
+        
+        #returns List of named functions
+        list(setMatrix = setMatrix, getMatrix = getMatrix,setInverseMatrix = setInverseMatrix,getInverseMatrix = getInverseMatrix)
+        
+        
 }
